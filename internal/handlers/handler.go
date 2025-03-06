171beaba96f0e509f90e17e5df4ffc19cdb25b6d
Lahiru Kariyawasam <lkksoftdev@gmail.com>
2025-03-06 12:49:11 +0530

    Validation error response refactored

--- internal/handlers/handler.go
+++ internal/handlers/handler.go
@@ -5,6 +5,7 @@ import (
 	appLogger "lt-app/internal/logger"
 	"lt-app/internal/services"
 	"lt-app/internal/utils"
+	"path/filepath"
 
 	"github.com/gofiber/fiber/v2"
 )
@@ -14,37 +15,43 @@ type RequestBody struct {
 	WebPageUrl string `json:"webPageUrl"`
 }
 
-func GetHome(c *fiber.Ctx) error {
-	return c.SendFile("./public/html/index.html")
+type RequestBodyValidationErr struct {
+	StatusCode int    `json:"statusCode"`
+	Error      string `json:"error"`
 }
 
-func validateRequestBody(c *fiber.Ctx, RLogger *slog.Logger) (RequestBody, string) {
+var rootPath = utils.GetProjectRoot()
+
+func GetHome(c *fiber.Ctx) error {
+	publicPath := filepath.Join(rootPath, "public")
+	return c.SendFile(filepath.Join(publicPath, "html/index.html"))
+}
+
+func validateRequestBody(c *fiber.Ctx, RLogger *slog.Logger) (RequestBody, *RequestBodyValidationErr) {
 	// Parse the request body
 	var body RequestBody
 	if err := c.BodyParser(&body); err != nil {
 		RLogger.Error("Failed to parse request body", slog.String("error", err.Error()))
-		return body, "Invalid request body"
+		return body, &RequestBodyValidationErr{StatusCode: fiber.StatusBadRequest, Error: "Invalid request body"}
 	}
 
 	// Validate the URL
 	if !utils.IsValidURL(body.WebPageUrl) {
 		RLogger.Warn("Invalid URL format", slog.String("url", body.WebPageUrl))
-		return body, "Invalid URL format"
+		return body, &RequestBodyValidationErr{StatusCode: fiber.StatusBadRequest, Error: "Invalid url format"}
 	}
 
-	return body, ""
+	return body, nil
 }
 
 func AnalyzeWebPage(c *fiber.Ctx) error {
 	RLogger := appLogger.RLoggerBuilder(c)
 
 	// Parse the request reqBody
-	reqBody, errMsg := validateRequestBody(c, RLogger)
+	reqBody, validationErr := validateRequestBody(c, RLogger)
 
-	if errMsg != "" {
-		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
-			"error": errMsg, "statusCode": "Invalid url format",
-		})
+	if validationErr != nil {
+		return c.Status(fiber.StatusBadRequest).JSON(validationErr)
 	}
 
 	stats, err := services.FetchWebPageStats(reqBody.WebPageUrl, RLogger)
